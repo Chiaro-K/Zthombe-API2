13f6171bfaea85ec2446ed8a90d7bc3973572139
EMEA\kmapuma <eq.mapuma@mercedes-benz.com>
2024-01-08 09:37:08 +0200

    create user api

--- Zthombe-API/Controllers/Command/UsersController.cs
+++ Zthombe-API/Controllers/Command/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Extensions;
 using Zthombe.Data.Constants;
+using Zthombe.Data.Models;
 using Zthombe_API.Models;
 
 namespace Zthombe_API.Controllers.Command
@@ -15,6 +16,27 @@ namespace Zthombe_API.Controllers.Command
         public UsersController(ZthombeContext zthombeContext)
         {
             this.zthombeContext = zthombeContext;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(UserModel), StatusCodes.Status200OK)]
+        //[ProducesResponseType(typeof(IEnumerable<ValidationError>), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserModel request)
+        {
+            if (request.Username != null && request.FirebaseUserId != null)
+            {
+                var user = new User()
+                {
+                    Username = request.Username,
+                    FirebaseUserId = request.FirebaseUserId
+                };
+
+                await zthombeContext.AddAsync(user);
+                var created = await zthombeContext.SaveChangesAsync();
+
+                return Ok(created);
+            }
+            return BadRequest();
         }
     }
 }
--- Zthombe.Data/Models/UserModel.cs
+++ Zthombe.Data/Models/UserModel.cs
@@ -6,5 +6,12 @@ namespace Zthombe.Data.Models
     public Guid UserId { get; set; }
     public string? Username { get; set; }
   }
-}
 
+
+}
+public class CreateUserModel
+{
+    public string Username { get; set; }
+    public string FirebaseUserId { get; set; }
+
+}
